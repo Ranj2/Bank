c44d170a454d5dc0b7427c4cac781dce6e383130
Ranj Mohammad <55746818+Ranj2@users.noreply.github.com>
2019-11-27 14:14:15 +0300

    Update Accounts.java

--- Bank_Command/src/Accounts.java
+++ Bank_Command/src/Accounts.java
@@ -1,6 +1,22 @@
 
 public class Accounts extends Main {
 	
+	String name;
+	String username;
+	String password;
+	Variables vb;
+	
+	
+	public Accounts(String name,String username, String password,int money){
+		
+		this.name = name;
+		vb.username = username;
+		vb.password = password;
+		vb.money = money;
+		
+		
+	}
+	
 	
 
 }
